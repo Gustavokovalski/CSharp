39018f960c0f9758ee6257cb6d61f30611500c29
Gustavo <gustavokovalski.saporiti@gmail.com>
2019-05-20 14:45:11 -0300

    FileStream e leitor StreamReader

---
+++ CSharp - Streams-(I-O)/ByteBankImportacaoExportacao/1_LidandoComFileStreamDiretamente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void LidandoComFileStreamDiretamente()
+        {
+            //var textoComQuebraDeLinha = "minha primeira linha \n minha segunda linha";
+
+            var enderecoDoArquivo = "contas.txt";
+
+            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024]; // É um buffer com 1kb
+            var numeroDeBytesLidos = -1; // (-1) pq é um valor que nunca vai ser retornado pelo READ, pois READ só retorna numero positivo ou 0, indicando que chegou no fim.
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                Console.WriteLine($"Bytes lidos:{numeroDeBytesLidos}");
+                EscreverBuffer(buffer, numeroDeBytesLidos);
+            }
+
+            fluxoDoArquivo.Close();
+
+
+        }
+
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
+        {
+            var utf8 = Encoding.Default;
+
+            var texto = utf8.GetString(buffer, 0, bytesLidos);
+            Console.Write(texto);
+
+            //foreach (var meuByte in buffer)
+            //{
+            //    Console.Write(meuByte);
+            //    Console.Write(" ");
+            //}
+        }
+    }
+}
--- CSharp - Streams-(I-O)/ByteBankImportacaoExportacao/Program.cs
+++ CSharp - Streams-(I-O)/ByteBankImportacaoExportacao/Program.cs
@@ -2,43 +2,31 @@
 using System.IO;
 using System.Text;
 
-namespace ByteBankImportacaoExportacao 
-{ 
-    class Program 
-    { 
-        static void Main(string[] args) 
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void Main(string[] args)
         {
-            //var textoComQuebraDeLinha = "minha primeira linha \n minha segunda linha";
-
             var enderecoDoArquivo = "contas.txt";
 
-            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-
-            var buffer = new byte[1024]; // É um buffer com 1kb
-            var numeroDeBytesLidos = -1; // (-1) pq é um valor que nunca vai ser retornado pelo READ, pois READ só retorna numero positivo ou 0, indicando que chegou no fim.
-
-            while(numeroDeBytesLidos != 0)
+            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            using (var leitor = new StreamReader(fluxoDeArquivo))
             {
-                fluxoDoArquivo.Read(buffer, 0, 1024);
-                EscreverBuffer(buffer);
+                while (!leitor.EndOfStream)
+                {
+                    var linha = leitor.ReadLine();
+                    //var linha = leitor.ReadToEnd(); USAR COM CUIDADO, pois faz a leitura ate o fim.
+                    //var linha = leitor.Read(); retorna int, o primeiro byte.
+                    Console.WriteLine(linha);
+                }
             }
+
+
+
 
             Console.ReadLine();
         }
 
-        static void EscreverBuffer(byte[] buffer)
-        {
-            var utf8 = Encoding.Default;
-
-            var texto = utf8.GetString(buffer);
-            Console.Write(texto);
-
-            //foreach (var meuByte in buffer)
-            //{
-            //    Console.Write(meuByte);
-            //    Console.Write(" ");
-            //}
-        }
     }
-} 
- 
+}
